90b1799b9aa27cad76f838150b12f581d699ad80
HumoyunXujaev <hujaevhumoyun01@gmail.com>
2024-07-22 12:54:55 +0500

    fix error

--- middleware.js
+++ middleware.js
@@ -33,7 +33,7 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
-  const { pathname, origin } = req.nextUrl;
+  const { pathname, origin, host } = req.nextUrl;
   const session = await getToken({ req, secret: process.env.JWT_SECRET });
 
   console.log(`Middleware called for path: ${pathname}`);
