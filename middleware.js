15253340f56c145aec824cd2bd715239748531fe
HumoyunXujaev <hujaevhumoyun01@gmail.com>
2024-07-22 18:56:18 +0500

    try

--- middleware.js
+++ middleware.js
@@ -1,34 +1,42 @@
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
-
 export async function middleware(req) {
   const { pathname, origin, host } = req.nextUrl;
   const session = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  console.log(`Path: ${pathname}, Origin: ${origin}, Host: ${host}`);
 
   const isAdminSubdomain = host.startsWith('admin.');
 
   if (pathname.startsWith('/admin') && !isAdminSubdomain) {
     if (!session) {
+      console.log(`Redirecting to signin from ${origin}/admin/dashboard`);
       return NextResponse.redirect(
         `${origin}/signin?callbackUrl=${origin}/admin/dashboard`
       );
     }
 
     if (session.role !== 'admin') {
-      return NextResponse.redirect(`${origin}`);
+      console.log(`Redirecting to ${origin} from non-admin role`);
+      return NextResponse.redirect(origin);
     }
 
+    console.log(`Redirecting to https://admin.telejkam.uz${pathname}`);
     return NextResponse.redirect(`https://admin.telejkam.uz${pathname}`);
   }
 
   if (isAdminSubdomain) {
     if (!session) {
+      console.log(`Redirecting to signin from admin subdomain`);
       return NextResponse.redirect(
         'https://www.telejkam.uz/signin?callbackUrl=https://admin.telejkam.uz/dashboard'
       );
     }
 
     if (session.role !== 'admin') {
+      console.log(
+        `Redirecting to https://www.telejkam.uz from admin subdomain`
+      );
       return NextResponse.redirect('https://www.telejkam.uz');
     }
 
@@ -36,12 +44,9 @@ export async function middleware(req) {
   }
 
   if (pathname.startsWith('/profile') && !session) {
+    console.log(`Redirecting to ${origin} from profile path`);
     return NextResponse.redirect(origin);
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/', '/admin/:path*', '/dashboard/:path*', '/profile/:path*'],
-};
