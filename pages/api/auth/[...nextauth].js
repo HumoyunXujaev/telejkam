553d16c4cefc08342597706e59a6802756ae8109
HumoyunXujaev <hujaevhumoyun01@gmail.com>
2024-07-22 15:09:43 +0500

    try to fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import bcrypt from 'bcrypt';
 import db from '@/utils/db';
+
 import clientPromise from '@/lib/mongodb';
 import { User } from '@/models/User';
 
@@ -16,6 +17,7 @@ db.connectDb();
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
+    // OAuth authentication providers...
     TwitterProvider({
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
@@ -46,8 +48,11 @@ export default NextAuth({
         const user = await User.findOne({ email });
 
         if (user) {
+          // Nếu user đã được tạo trước đó trong database, gọi hàm SignInUser với
+          //đối số là object chứa password và user
           return SignInUser({ password, user });
         } else {
+          // Nếu user không tồn tại trong database, return error
           throw new Error('This email does not exist');
         }
       },
@@ -55,18 +60,13 @@ export default NextAuth({
   ],
   callbacks: {
     async session({ session, token }) {
+      //Sub của Token là field chứa _id của user trong Database.
       let user = await User.findById(token.sub);
+      //Bổ sung thêm thông tin cho user của session
       session.user.id = token.sub || user._id.toString();
       session.user.role = user.role || 'user';
       token.role = user.role || 'user';
       return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        token.sub = user.id;
-        token.role = user.role;
-      }
-      return token;
     },
   },
   pages: {
@@ -74,35 +74,18 @@ export default NextAuth({
   },
   session: {
     strategy: 'jwt',
+    maxAge: 24 * 60 * 60, // 1 день
+    updateAge: 24 * 60 * 60, // 1 день
   },
   cookies: {
     sessionToken: {
-      name: `__Secure-next-auth.session-token`,
+      name: `next-auth.session-token`,
       options: {
+        domain: '.telejkam.uz', // Поддержка кросс-доменного доступа
+        path: '/',
         httpOnly: true,
         sameSite: 'lax',
-        path: '/',
         secure: process.env.NODE_ENV === 'production',
-        domain: '.telejkam.uz', // Set the cookie domain to the root domain
-      },
-    },
-    callbackUrl: {
-      name: `__Secure-next-auth.callback-url`,
-      options: {
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-        domain: '.telejkam.uz', // Set the cookie domain to the root domain
-      },
-    },
-    csrfToken: {
-      name: `__Host-next-auth.csrf-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-        domain: '.telejkam.uz', // Set the cookie domain to the root domain
       },
     },
   },
@@ -114,9 +97,11 @@ const SignInUser = async ({ password, user }) => {
     throw new Error('Please enter your password');
   }
 
-  const isMatch = await bcrypt.compare(password, user.password);
+  // Dùng compare của bcrypt để so sánh 2 password
+  const textPassword = await bcrypt.compare(password, user.password);
 
-  if (!isMatch) {
+  // Nếu password không khớp
+  if (!textPassword) {
     throw new Error('Email or password is wrong');
   }
   return user;
