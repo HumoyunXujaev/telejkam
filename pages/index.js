efc675470f73cb48a5ef78412509825a5d1c91ff
HumoyunXujaev <hujaevhumoyun01@gmail.com>
2025-02-16 21:18:07 +0500

    try to make faster

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
 import Main from '@/components/Home/Main';
 import { Product } from '@/models/Product';
 import db from '@/utils/db';
@@ -8,16 +6,10 @@ import { Category } from '@/models/Category';
 import styled from '../styles/Home.module.scss';
 import AllProducts from '@/components/Home/AllProducts';
 import AnimateWrapper from '@/components/AnimateWrapper';
-import { useRouter } from 'next/router';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React, { useCallback, useEffect } from 'react';
-import { se } from 'date-fns/locale';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-const MemoizedHeader = React.memo(Header);
-const MemoizedAllProducts = React.memo(AllProducts);
 
 export default function Home({
-  country,
   products,
   flashDeals,
   featuredProducts,
@@ -25,8 +17,6 @@ export default function Home({
   featuredCategories,
   settings,
 }) {
-  const router = useRouter();
-
   // SET THE PRODUCTS TO THE SESSION STORAGE
   useEffect(() => {
     const fetchData = async () => {
@@ -46,7 +36,6 @@ export default function Home({
 
   return (
     <>
-      {/* <MemoizedHeader country={country} searchHandler={searchHandler} /> */}
       <div className={styled.home}>
         <div className={styled.container}>
           <Main
@@ -58,18 +47,19 @@ export default function Home({
             settings={settings}
           />
           <AnimateWrapper>
-            <MemoizedAllProducts products={products} />
+            <AllProducts products={products} />
           </AnimateWrapper>
         </div>
       </div>
-      {/* <Footer /> */}
     </>
   );
 }
 
 export async function getStaticProps({ locale }) {
   await db.connectDb();
+
   const settings = await Settings.findOne({}).lean();
+
   let products = await Product.find()
     .sort({ createdAt: -1 })
     .select('category brand name rating slug subProducts _id shipping')
